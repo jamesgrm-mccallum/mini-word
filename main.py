f7673119424b4807b0744929461f663ea75c1b1e
xshadow989x <jamesgrm081@gmail.com>
2024-08-23 22:27:16 -0400

    Co-authored-by: hasan-a5 <hasan-a5@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 """
 Entry point of the program. 
 """
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
+from docx import Document
+
 
         
+def save_to_file() -> None:
+    """Takes text from <textbox> and writes it into 
+    a .docx and .txt file"""
+    text = textbox.get("1.0", tk.END)
+    
+    document = Document()
+    document.add_paragraph(text)
+    document.save("text.docx")
+    
+    with open('text.txt', 'w') as f:
+        f.writelines(text)
 
 
-
-
+    
+def load_from_file() -> None:
+    """Opens <text.txt> and writes it's content into <textbox>"""
+    with open('text.txt', 'r') as f:
+        text = f.readlines()
+        textbox.insert("1.0", '\n'.join(text))
+                
 
 if __name__ == "__main__":
-    # tkinter setup
     
-    root = Tk()
+    root = tk.Tk()
     root.title("Mini-Word")
-    root.geometry("1280x720")
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=0)
-    textbox = Text(root, height = 52, width = 52, font = ('Arial', 16)).grid(column=1,row=1)
-
+    root.geometry("1920x1024")
     
-    while True:
-        # get user input (keys)
-        
-        
-        
-        # update the backend (storing data)
-        
-        
-        # display to screen
-        root.update_idletasks()
-        root.update()
+    page = tk.Frame(root, background='red')
+    page.grid()
+    
+    
+    top_bar = tk.Frame(page)
+    top_bar.grid(column=0,row=0)
+    top_bar.grid()
+    
+    
+    submitbutton = tk.Button(top_bar, width=10, height=1, text='SAVE', command=save_to_file)
+    submitbutton.grid(row=0, column=0)
+    insertbutton = tk.Button(top_bar, width=10, height=1, text='LOAD', command=load_from_file)
+    insertbutton.grid(row=0, column=1)
+    textbox = tk.Text(page, height = 20, width = 20, font = ('Arial', 16))
+    textbox.grid(column=0, row=1)
+    root.mainloop()
     
     
     
