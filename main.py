6e520b024052afb002ee9e2749d3fccc07a05bd5
xshadow989x <jamesgrm081@gmail.com>
2024-08-23 21:19:26 -0400

    Co-authored-by: hasan-a5 <hasan-a5@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 """
 Entry point of the program. 
 """
+from tkinter import *
+from tkinter import ttk
+
+        
+
+
+
 
 
 if __name__ == "__main__":
-    # pygame setup
-    import pygame
-    pygame.init()
-    canvas = pygame.display.set_mode((1280, 720))
-    pygame.display.set_caption("Mini-Word")
+    # tkinter setup
     
-    # main loop
-    exit = False
-    while not exit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit = True
-        pygame.display.update()
+    root = Tk()
+    root.title("Mini-Word")
+    root.geometry("1280x720")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=0)
+    textbox = Text(root, height = 842, width = 595, font = ('Arial', 16)).grid(column=1,row=1)
+
+    
+    while True:
+        # get user input (keys)
+        
+        
+        
+        # update the backend (storing data)
+        
+        
+        # display to screen
+        root.update_idletasks()
+        root.update()
+    
+    
+    
